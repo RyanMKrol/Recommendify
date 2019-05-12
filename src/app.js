cee54dbc9155f8548c0cecf1fcb48368f2889a33
Ryan Krol <ryankr@hotmail.co.uk>
2019-05-12 19:54:31 +0100

    Clear up boilerplate code
    * Removing the default API calls and refactoring a few conditionals

--- src/app.js
+++ src/app.js
@@ -34,59 +34,41 @@ app.get('/login', function(req, res) {
 })
 
 app.get('/callback', function(req, res) {
-
-  // your application requests refresh and access tokens
-  // after checking the state parameter
-
   const code = req.query.code
   const state = req.query.state
   const storedState = req.cookies ? req.cookies[stateKey] : null
 
   if (!state || state !== storedState) {
-      res.status(500).send('Internal Server Error');
-  } else {
-    res.clearCookie(stateKey)
-    const authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
-      },
-      json: true
+      res.status(500).send('Internal Server Error')
+      return
+  }
+
+  res.clearCookie(stateKey)
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
+    },
+    json: true
+  }
+
+  request.post(authOptions, function(error, response, body) {
+    if (error || response.statusCode !== 200) {
+        res.status(500).send('Invalid Token');
+        return
     }
 
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
+    const access_token = body.access_token
+    const refresh_token = body.refresh_token
 
-        const access_token = body.access_token,
-            refresh_token = body.refresh_token
+    // store the token and start doing something with it
 
-        const options = {
-          url: 'https://api.spotify.com/v1/me/player',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        }
-
-        // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-          console.log(body)
-        })
-
-        // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }))
-      } else {
-        res.status(500).send('Invalid Token');
-      }
-    })
-  }
+  })
 })
 
 app.listen(8000, () => {
