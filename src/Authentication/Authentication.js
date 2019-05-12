3e2f884faef234aa74e69ab02e5e863b56035cb6
Ryan Krol <ryankr@hotmail.co.uk>
2019-05-12 22:05:05 +0100

    Compartmentalise code into auth module
    * Auth is fully handled now. Just need to build the actual
    app now.

---
+++ src/Authentication/Authentication.js
@@ -0,0 +1,81 @@
+import config from 'config'
+import querystring from 'querystring'
+import requestLib from 'request-promise-native'
+import uuid from 'uuid/v1'
+
+const CLIENT_ID = 'a9333632c14a45b0ad372b5ab7a8afef'
+const CLIENT_SECRET = config.get('clientSecret')
+const REDIRECT_URL = 'http://localhost:8000/callback'
+const AUTH_STATE_KEY = 'spotify_auth_state'
+const PERMISSIONS_LIST = 'user-read-private user-read-email user-read-playback-state playlist-modify-public playlist-modify-private'
+
+// user permits us to act on their behalf
+function requestInitialAuth(httpResponse) {
+    const state = uuid()
+
+    httpResponse.cookie(AUTH_STATE_KEY, state)
+
+    const authoriseParams = querystring.stringify({
+      response_type: 'code',
+      client_id: CLIENT_ID,
+      scope: PERMISSIONS_LIST,
+      redirect_uri: REDIRECT_URL,
+      state: state
+    })
+
+    httpResponse.redirect(`https://accounts.spotify.com/authorize?${authoriseParams}`)
+}
+
+// double handshake - we use a code passed back from Spotify to fetch the access tokens
+async function requestApiTokens(httpRequest, httpResponse) {
+  if (isInvalidState(httpRequest, httpResponse)) return
+
+  httpResponse.clearCookie(AUTH_STATE_KEY)
+  const authOptions = buildAuthOptions(httpRequest)
+
+  return requestLib.post(authOptions)
+  .then((response) => {
+    return ({
+      "accessToken": response.access_token,
+      "refreshToken": response.refresh_token
+    })
+  }).catch((error) => {
+    httpResponse.status(500).send('Invalid Token')
+    throw new Error('Invalid Token provided')
+  })
+}
+
+function isInvalidState(httpRequest, httpResponse) {
+  const state = httpRequest.query.state
+  const storedState = httpRequest.cookies ? httpRequest.cookies[AUTH_STATE_KEY] : null
+
+  if (!state || state !== storedState) {
+    httpResponse.status(500).send('Internal Server Error')
+    return true
+  }
+
+  return false
+}
+
+function buildAuthOptions(httpRequest) {
+  const code = httpRequest.query.code
+  const authToken = new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')
+
+  return {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: REDIRECT_URL,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': `Basic ${authToken}`
+    },
+    json: true
+  }
+}
+
+export {
+  requestInitialAuth,
+  requestApiTokens
+}
--- src/app.js
+++ src/app.js
@@ -1,70 +1,27 @@
-import config from 'config'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import express from 'express'
-import querystring from 'querystring'
 import request from 'request'
-import uuid from 'uuid/v1'
+import * as authLib from './Authentication'
 
 const app = express()
-const clientId = 'a9333632c14a45b0ad372b5ab7a8afef'
-const clientSecret = config.get('clientSecret');
-const redirect_uri = 'http://localhost:8000/callback'
-const stateKey = 'spotify_auth_state'
-const scope = 'user-read-private user-read-email user-read-playback-state playlist-modify-public playlist-modify-private'
 
 app.use(express.static(__dirname + './../public'))
-   .use(cors())
-   .use(cookieParser())
+  .use(cors())
+  .use(cookieParser())
 
+// initial permissions fetching
 app.get('/login', function(req, res) {
-
-  const state = uuid()
-  res.cookie(stateKey, state)
-
-  const authoriseParams = querystring.stringify({
-    response_type: 'code',
-    client_id: clientId,
-    scope: scope,
-    redirect_uri: redirect_uri,
-    state: state
-  })
-
-  res.redirect(`https://accounts.spotify.com/authorize?${authoriseParams}`)
+  authLib.requestInitialAuth(res)
 })
 
-app.get('/callback', function(req, res) {
-  const code = req.query.code
-  const state = req.query.state
-  const storedState = req.cookies ? req.cookies[stateKey] : null
+app.get('/callback', async function(req, res) {
+  try {
+    // getting specific tokens for API requests 
+    const tokens = await authLib.requestApiTokens(req, res)
 
-  if (!state || state !== storedState) {
-      res.status(500).send('Internal Server Error')
-      return
-  }
-
-  res.clearCookie(stateKey)
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    form: {
-      code: code,
-      redirect_uri: redirect_uri,
-      grant_type: 'authorization_code'
-    },
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
-    },
-    json: true
-  }
-
-  request.post(authOptions, function(error, response, body) {
-    if (error || response.statusCode !== 200) {
-        res.status(500).send('Invalid Token');
-        return
-    }
-
-    const access_token = body.access_token
-    const refresh_token = body.refresh_token
+    const access_token = tokens.accessToken
+    const refresh_token = tokens.refreshToken
 
     const options = {
       url: 'https://api.spotify.com/v1/me',
@@ -74,31 +31,31 @@ app.get('/callback', function(req, res) {
 
     // use the access token to access the Spotify Web API
     request.get(options, function(error, response, body) {
+      console.log('WHATS GOING ON HERE')
       console.log(body)
       const userId = body.id
 
       const playlistApiOptions = {
-          url: `https://api.spotify.com/v1/users/${userId}/playlists`,
-          headers: {
-              'Authorization': `Bearer ${access_token}`,
-              'Content-Type': 'application/json'
-          },
-          body: {
-              "name": "New Playlist",
-              "description": "New playlist description",
-              "public": true
-          },
-          json: true
+        url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+        headers: {
+          'Authorization': `Bearer ${access_token}`,
+          'Content-Type': 'application/json'
+        },
+        body: {
+          "name": "New Playlist",
+          "description": "New playlist description",
+          "public": true
+        },
+        json: true
       }
 
       request.post(playlistApiOptions, function(error, response, body) {
           res.status(200).send('Well done');
       })
     })
-
-    // store the token and start doing something with it
-
-  })
+  } catch(err) {
+    console.log(err)
+  }
 })
 
 app.listen(8000, () => {
